2bc4bbf43ea6c92f9e3bc1d894de90375ec9f2b4
Vladimir <Vladimir.Zivkovic@nbcuni.com>
2020-06-02 14:38:21 +0200

    worker

--- map-marketingcloud-worker/app/service/api/v1/controller/product_data_controller.py
+++ map-marketingcloud-worker/app/service/api/v1/controller/product_data_controller.py
@@ -35,3 +35,51 @@ class ProductDataController:
                 return response, 409
             else:
                 return response, 500
+
+
+class ConnectionController:
+    
+    def __init__(self, context: ServiceContext = None):
+        if context is None:
+            context = build_context('service')
+        self.context = context
+        self.work_queue = WorkQueue(self.context.WORK_QUEUE_CONTEXT)
+        self.queue_key = QueueKeys.PRODUCT_DATA
+
+    def post(self) -> (OrderedDict, int):
+        """
+        Routes the request to the correct queue method and returns the status code and error message as applicable.
+
+        :return: (status_code: int, error: str)
+        """
+        success, error = self.work_queue.put(self.queue_key)
+        if success:
+            response, model = build_response('ok')
+            return ResponseEnvelope(model).create_single_result_response(response), 200
+
+
+class WorkerProcessController:
+    
+    def __init__(self, context: ServiceContext = None):
+        if context is None:
+            context = build_context('service')
+        self.context = context
+        self.work_queue = WorkQueue(self.context.WORK_QUEUE_CONTEXT)
+        self.queue_key = QueueKeys.PRODUCT_DATA
+
+    def post(self) -> (OrderedDict, int):
+        """
+        Routes the request to the correct queue method and returns the status code and error message as applicable.
+
+        :return: (status_code: int, error: str)
+        """
+        success, error = self.work_queue.put(self.queue_key)
+        if success:
+            response, model = build_response('ok')
+            return ResponseEnvelope(model).create_single_result_response(response), 200
+        else:
+            response = ResponseEnvelope(dict()).create_error_response(error)
+            if error == ErrorMessages.LOCKED_EXISTS or error == ErrorMessages.UNLOCKED_EXISTS:
+                return response, 500
+            else:
+                return response, 503
--- map-marketingcloud-worker/app/service/api/v1/marketing_cloud.py
+++ map-marketingcloud-worker/app/service/api/v1/marketing_cloud.py
@@ -2,6 +2,8 @@ from flask_restplus import Namespace, Resource
 
 from config import title, version
 from service.api.v1.controller.product_data_controller import ProductDataController
+from service.api.v1.controller.product_data_controller import ConnectionController
+from service.api.v1.controller.product_data_controller import WorkProcessController
 """
 This file defines endpoints for the Sales Force Marketing Cloud namespace API
 """
@@ -19,3 +21,19 @@ class ProductData(Resource):
     def post():
         """ Activate the process of loading Sales Force Marketing Cloud API data into the database """
         return ProductDataController().post()
+
+@marketing_cloud_ns.route('/admin/health', methods=['POST'])
+class ConnectionData(Resource):
+    """Endpoints tied to the product data workflow with Sales Force Marketing Cloud"""
+    @staticmethod
+    def post():
+        """ Activate the process of loading Sales Force Marketing Cloud API data into the database """
+        return ConnectionController().post()
+
+@marketing_cloud_ns.route('/admin/ready', methods=['POST'])
+class WorkProcessData(Resource):
+    """Endpoints tied to the product data workflow with Sales Force Marketing Cloud"""
+    @staticmethod
+    def post():
+        """ Activate the process of loading Sales Force Marketing Cloud API data into the database """
+        return WorkProcessController().post()
